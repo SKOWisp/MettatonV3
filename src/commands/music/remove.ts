f3af0271fd95a28fa1cb7676edba33240699408b
Román T. Vidal Tamayo <58921058+SKOWisp@users.noreply.github.com>
2024-02-10 14:59:17 -0600

    Added \remove command

---
+++ src/commands/music/remove.ts
@@ -0,0 +1,60 @@
+import { GuildMember, SlashCommandBuilder } from 'discord.js';
+import { LooseCommandInteraction } from '../../LooseClient';
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('remove')
+		.setDescription('Removes a song from the /queue.')
+		.addIntegerOption(option =>
+			option.setName('position')
+				.setDescription('Queue position of the song to remove.')
+				.setRequired(true)),
+	async execute(interaction: LooseCommandInteraction) {
+
+		const channel = (interaction.member as GuildMember).voice.channel;
+		const serverQueue = interaction.client.queues.get(interaction.guildId!);
+
+		// Return if there is no severQueue object
+		if (!serverQueue) return interaction.reply({ content: `There is nothing playing on ${interaction.guild!.name}. (?)`, ephemeral: true });
+
+		if (!channel || channel.id !== serverQueue.voiceConnection.joinConfig.channelId) {
+			return interaction.reply({ content: 'Join the voice channel first!.', ephemeral: true });
+		}
+
+		/*
+			Parse position
+			reject negative values and 0
+		*/
+		let pos: number = (interaction.options as any).getInteger('position');
+		pos = ((Number.isNaN(pos) || pos === 0) ? 1 : pos);
+		pos = (pos < 0 ? Math.abs(pos) : pos);
+
+		const queueLength = serverQueue.queue.length;
+
+		// Do nothing if there's nothing to do
+		if (pos - 1 > queueLength) {
+			await interaction.reply({ content: 'https://media1.tenor.com/m/TFFBKrvBc_sAAAAd/zoku-owarimonogatari-serpent.gif' });
+			return interaction.channel!.send('*\\*zzZZZzzz...\\** (nadeko doesn\'t have anything to remove)');
+		}
+
+		// Stop current song if pos is 1
+		// Leave channel if no songs left
+		if (pos === 1) {
+			serverQueue.audioPlayer.stop();
+
+			if (queueLength === 0) {
+				serverQueue.eraseQueue();
+				interaction.client.queues.delete(interaction.guildId!);
+			}
+
+			await interaction.reply({ content: 'https://media1.tenor.com/m/dRpsv_G8zHwAAAAC/monogatari-monogatari-series.gif' });
+			return interaction.channel!.send('use /skip dummy...');
+		}
+
+		// Remove song in /queue
+		const songName = serverQueue.queue[pos - 2].name;
+		serverQueue.queue.splice(pos - 2, 1);
+
+		return interaction.reply({ content: `Removed song #${pos}: ${songName}.` });
+	},
+};
