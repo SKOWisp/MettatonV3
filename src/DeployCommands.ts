ab99c600ce55c72ae1efe961290edca79d64e2cb
Román T. Vidal Tamayo <58921058+SKOWisp@users.noreply.github.com>
2024-07-12 16:17:22 -0600

    Update DeployCommands.ts
    Applies commands globally

--- src/DeployCommands.ts
+++ src/DeployCommands.ts
@@ -36,18 +36,18 @@ const rest = new REST().setToken(process.env.BOT_TOKEN!);
 		console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
-
+		/*
 		const data: any = await rest.put(
 			Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!),
 			{ body: commands },
 		);
+		*/
 
-		/*
 		const data: any = await rest.put(
 			Routes.applicationCommands(process.env.CLIENT_ID!),
 			{ body: commands },
 		);
-		*/
+
 
 		console.log(`Successfully reloaded ${data!.length} application (/) commands.`);
 	}
