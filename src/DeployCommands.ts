af45e1eb82e52d7d9a97c770b43ccb3285794539
SKOWisp <58921058+SKOWisp@users.noreply.github.com>
2024-12-10 17:16:21 -0600

    Added ability to add bot-owner-specific commands

--- src/DeployCommands.ts
+++ src/DeployCommands.ts
@@ -3,29 +3,47 @@ import 'dotenv/config';
 import fs from 'node:fs';
 import path from 'node:path';
 
-const commands = [];
-
 // Grab all the command files from the commands directory
 const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+const generalCommandFolders = fs.readdirSync(foldersPath);
 
-for (const folder of commandFolders) {
-	// Grab all the command files from the commands directoryr
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+// Remove bot owner commands directory
+const ownerCommandFolders = generalCommandFolders.splice(generalCommandFolders.indexOf('owner'), 1);
 
-	// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			commands.push(command.data.toJSON());
-		}
-		else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+/**
+ * Reads command files from command folders
+ * @param {string[]} folders Folder paths that contain commands
+ * @returns {never[]} Array of commands
+ * @private
+ */
+function ReadCommandFolders(folders: string[]): never[] {
+	const commands: never[] = [];
+
+	for (const folder of folders) {
+		
+
+		// Grab all the command files from the commands directoryr
+		const commandsPath = path.join(foldersPath, folder);
+		const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.ts'));
+
+		// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+		for (const file of commandFiles) {
+			const filePath = path.join(commandsPath, file);
+			const command = require(filePath);
+			if ('data' in command && 'execute' in command) {
+				commands.push(command.data.toJSON() as never);
+			}
+			else {
+				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+			}
 		}
 	}
+
+	return commands
 }
+
+const generalCommands = ReadCommandFolders(generalCommandFolders);
+const ownerCommands = ReadCommandFolders(ownerCommandFolders);
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(process.env.BOT_TOKEN!);
@@ -33,23 +51,22 @@ const rest = new REST().setToken(process.env.BOT_TOKEN!);
 // Depoly commands in guild
 (async () => {
 	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+		console.log(`Started refreshing ${generalCommands.length} general application and ${ownerCommands.length} bot owner (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		/*
-		const data: any = await rest.put(
+		// Refresh owner commands in specific guild
+		const data1: any = await rest.put(
 			Routes.applicationGuildCommands(process.env.CLIENT_ID!, process.env.GUILD_ID!),
-			{ body: commands },
+			{ body: ownerCommands },
 		);
-		*/
-
-		const data: any = await rest.put(
+		
+		// Refresh all other commands
+		const data2: any = await rest.put(
 			Routes.applicationCommands(process.env.CLIENT_ID!),
-			{ body: commands },
+			{ body: generalCommands },
 		);
 
 
-		console.log(`Successfully reloaded ${data!.length} application (/) commands.`);
+		console.log(`Successfully reloaded ${data2.length} general application and ${data1.length} bot owner (/) commands.`);
 	}
 	catch (error) {
 		// And of course, make sure you catch and log any errors!
